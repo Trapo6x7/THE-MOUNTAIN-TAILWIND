5405483828c30c4670b23b5c7a6467b024bb39d5
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-04 14:02:28 +0100

    ADD:ABOUT+BLOG

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,23 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
-      colors:{
-        neutral:{
-          'off-white': 'hsl(36, 100%, 99%)',
-          'cyan' : '#36C4C5',
-          'red':'#F52257',
-      },
+      colors: {
+        neutral: {
+          "off-white": "hsl(36, 100%, 99%)",
+          'cyan': "#36C4C5",
+          'red': "#F52257",
+          'grey': "#545454",
+        },
       },
       fontFamily: {
         Roboto: ["Roboto", "sans-serif"],
         Rokkitt: ["Rokkitt", "sans-serif"],
       },
       backgroundImage: {
-        'hero-img': "url('../images/bg_header.jpg')",
+        "hero-img": "url('../images/bg_header.jpg')",
+        "nuage-img": "url('../images/bg_nuage.jpg')",
+        "news-img": "url('../images/bg_newsletter.jpg')",
+        "photo-img": "url('../images/bg_appareil_photo.jpg')",
       },
     },
   },
