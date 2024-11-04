b5fac38587cda1dd3deaec05c07fdd57d0ebdb7e
Trapo6x7 <matoufdanslezoo@gmail.com>
2024-11-04 11:05:24 +0100

    ADD:HEASER

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        neutral:{
+          'off-white': 'hsl(36, 100%, 99%)',
+          'cyan' : '#36C4C5',
+          'red':'#F52257',
+      },
+      },
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+        Rokkitt: ["Rokkitt", "sans-serif"],
+      },
+      backgroundImage: {
+        'hero-img': "url('../images/bg_header.jpg')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
